265d9c259ea3aa6fb6db97541e6c7e180a465795
Vadiksimf <vadiksimf88@gmail.com>
2022-10-15 16:49:34 +0400

    Update realEstateAgency.sol

--- realEstateAgency.sol
+++ realEstateAgency.sol
@@ -24,122 +24,187 @@ Requirements:
 - при успешном переводе средств сменяется собственник, объявление становится неактуальнымдарить и продавать собственность с обременением нельзя
 */
 
-contract SoldRealEstate {
-    uint256 id;
-    address recepient;
-
-    constructor (address _recepient, uint256 _id) {
-        recepient = _recepient;
-        id = _id;
-    }
-}
-
-contract PresentRealEstate {
-    uint256 id;
-    address recepient;
-
-    constructor (address _recepient, uint256 _id) {
-        recepient = _recepient;
-        id = _id;
-    }
-}
-
 contract Agency {
-    address admin;
-
-    struct realEstateModel {
-        address owner;
-        string location;
-        uint256 square;
-        string encumbrance;
-    }
-
-    mapping(uint256 => realEstateModel) realEstate;
-    uint256[] realEstateIds;
-
-    PresentRealEstate public present;
-    SoldRealEstate public sold;
+    address internal admin;
 
     constructor() {
         admin = msg.sender;
-        realEstateModel storage newNewRealEstate = realEstate[1];
+        RealEstateModel storage newNewRealEstate = realEstate[1];
         newNewRealEstate.owner = msg.sender;
         newNewRealEstate.location = "Taganrog city, Chehova street 2";
         newNewRealEstate.square = 1000;
-        newNewRealEstate.encumbrance = "";
+        newNewRealEstate.restrictions = false;
+        newNewRealEstate.valid = true;
+    }
+
+    struct RealEstateModel {
+        address owner;
+        string location;
+        uint256 square;
+        bool restrictions;
+        bool valid;
+    }
+
+    struct PresentModel {
+        uint256 estateId;
+        address to;
+        bool accepted;
+    }
+
+    struct OnSaleModel {
+        uint256 estateId;
+        address seller;
+        uint price;
+        bool isSold;
+    }
+
+    mapping(uint256 => RealEstateModel) realEstate;
+    uint256[] realEstateIds;
+    mapping(uint256 => PresentModel) presents;
+    uint256[] presentEstateIds;
+    mapping(uint256 => OnSaleModel) onSaleAds;
+    uint256[] onSaleIds;
+
+    function compareStrings(string memory a, string memory b) private view returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+
+    function isAdmin() private view returns (bool) {
+        return msg.sender == admin;
+    }
+
+    function validateAdminActions (uint id) 
+            private view returns (bool, string memory) {
+
+        RealEstateModel storage obj = realEstate[id];
+        if(msg.sender != admin) return (false, "Denied! You are not an admin!");
+        if(!obj.valid) return (false, "Real Estate doesn't exist");
+
+        return (true, '');
+    }
+
+    function validateUsersActions (uint256 id) private view returns (bool, string memory) {
+        RealEstateModel storage obj = realEstate[id];
+        if (!obj.valid) return (false, "Real Estate doesn't exist");
+        if (msg.sender != obj.owner) return (false, "You are not allowed to perform this action!");
+        if (obj.restrictions) return (false, "This property is restricted");
+        
+        return (true, "");
+    }
+
+    function validateIfEstateIsAccessible (uint256 id) private 
+            view returns (bool, string memory) {
+        RealEstateModel storage estate = realEstate[id];
+        if (!estate.valid) return (false, "Real Estate doesn't exist");
+        if (estate.restrictions) return (false, "This property is restricted");
+
+        return (true, '');
     }
 
     function registerRealEstate(address owner, string memory location, 
-        uint256 square, string memory encumbrance, uint256 id) public {
+            uint256 square, bool restrictions, uint256 id) public {
+        
+        require(isAdmin(), "Denied! You are not an admin!");
 
-        require(msg.sender != admin, "Denied! You are not an admin!");
-        realEstateModel storage newNewRealEstate = realEstate[id];
+        RealEstateModel storage newNewRealEstate = realEstate[id];
         newNewRealEstate.owner = owner;
         newNewRealEstate.location = location;
         newNewRealEstate.square = square;
-        newNewRealEstate.encumbrance = encumbrance;
+        newNewRealEstate.restrictions = restrictions;
+        newNewRealEstate.valid = true;
     }
     
     function getRealEstate(uint256 id) 
         public view returns (address, string memory, 
-        uint256, string memory) {
+            uint256, bool) {
         require(bytes(realEstate[id].location).length != 0, "Real Estate doesn't exist");
 
         return (realEstate[id].owner, realEstate[id].location, 
-        realEstate[id].square, realEstate[id].encumbrance);
-    }
-
-    function isAdmin() private returns (bool) {
-        return msg.sender == admin;
+        realEstate[id].square, realEstate[id].restrictions);
     }
 
     function changeOwner (uint256 id, address owner) public {
-        require(isAdmin(), "Denied! You are not an admin!");
-        realEstateModel storage obj = realEstate[id];
-        require(bytes(obj.location).length != 0, "Real Estate doesn't exist");
-        if (owner != address(0)) obj.owner = owner;
+        (bool success, string memory message) = validateAdminActions(id);
+        require(success, message);
+
+        if (owner != address(0)) realEstate[id].owner = owner;
     }
 
     function changeSquare (uint256 id, uint256 square) public {
-        require(isAdmin(), "Denied! You are not an admin!");
-        realEstateModel storage obj = realEstate[id];
-        require(bytes(obj.location).length != 0, "Real Estate doesn't exist");
-        if (square != 0) obj.square = square;
+        (bool success, string memory message) = validateAdminActions(id);
+        require(success, message);
+
+        if (square != 0) realEstate[id].square = square;
     }
 
-    function changeEncumbrance (uint256 id, string memory encumbrance) public {
-        require(isAdmin(), "Denied! You are not an admin!");
-        realEstateModel storage obj = realEstate[id];
-        require(bytes(obj.location).length != 0, "Real Estate doesn't exist");
-        if (bytes(encumbrance).length != 0) obj.encumbrance = encumbrance;
+    function changeRestrictions (uint256 id) public {
+      (bool success, string memory message) = validateAdminActions(id);
+        require(success, message);
+
+        realEstate[id].restrictions = !realEstate[id].restrictions;
     }
 
-    function changeEntireRealEstateInstance(address owner, string memory location, 
-        uint256 square, string memory encumbrance, uint256 id) public
-        returns (address, string memory, uint256, string memory) {
-
-        require(msg.sender != admin, "Denied! You are not an admin!");
-        realEstateModel storage obj = realEstate[id];
-        require(bytes(obj.location).length != 0, "Real Estate doesn't exist");
-
-        if (owner != address(0)) obj.owner = owner;
-        if (bytes(location).length != 0) obj.location = location;
-        if (square != 0) obj.square = square;
-        if (bytes(encumbrance).length != 0) obj.encumbrance = encumbrance;
-
-        return (obj.owner, obj.location, obj.square, obj.encumbrance);
-    }
-
-    function soldRealEstate (uint256 id, address recepient) public {
-        realEstateModel storage obj = realEstate[id];
-        require(bytes(obj.location).length != 0, "Real Estate doesn't exist");
-        require(msg.sender == obj.owner, "You are not allowed to perform this action!");
-        require(bytes(obj.encumbrance).length != 0, "This property is restricted");
+    function presentRealEstate (uint256 id, address recepient) public {
+        (bool success, string memory message) = validateUsersActions(id);
+        require(success, message);
         
-        sold = new SoldRealEstate(recepient, id);
+        PresentModel storage newPresent = presents[id];  
+        newPresent.estateId = id;
+        newPresent.to = recepient;
+        newPresent.accepted = false;
     }
 
-    function presentRealEstate (uint256 id) public {
+    function acceptPresent (uint256 presentId) public {
+        PresentModel storage present = presents[presentId];
+        RealEstateModel storage estate = realEstate[presentId];
+        require(estate.valid, "Real Estate doesn't exist");
+        require(!present.accepted, "Present already accepted by user!");
+        require(present.to != msg.sender, "You are not allowed to perform this action!");
+        require(!estate.restrictions, "This property is restricted");
 
+        estate.owner = msg.sender;
+        present.accepted = true;
+    }
+
+    function checkPresent (uint256 id) public view returns (uint256, address, bool) {
+        return (presents[id].estateId, presents[id].to, presents[id].accepted);
+    }
+
+   function sellRealEstate (uint256 id, uint256 price) public {
+        (bool success, string memory message) = validateUsersActions(id);
+        require(success, message);
+        
+        OnSaleModel storage newOnSaleAd = onSaleAds[id];  
+        newOnSaleAd.estateId = id;
+        newOnSaleAd.seller = msg.sender;
+        newOnSaleAd.price = price;
+        newOnSaleAd.isSold = false;
+    }
+
+    event Deposit(address sender, uint amount);
+    event Withdrawal(address receiver, uint amount);
+    event Transfer(address sender, address receiver, uint amount);
+
+    function buyRealEstate (uint256 estateId) public payable {
+        OnSaleModel storage onSaleEstate = onSaleAds[estateId];
+        RealEstateModel storage estate = realEstate[estateId];
+        require(estate.valid, "Real Estate doesn't exist");
+        require(!onSaleEstate.isSold, "Real Estate already sold!");
+        require(!estate.restrictions, "This property is restricted");
+        require(msg.value != onSaleEstate.price, "Wrong amount of money");
+
+        require(address(msg.sender).balance >= onSaleEstate.price, "Insufficient funds");
+        // emit Transfer(msg.sender, onSaleEstate.seller, onSaleEstate.price);
+
+        address payable seller = payable(onSaleEstate.seller);
+        (bool success, ) = seller.call{value: onSaleEstate.price}("");
+        require(success, "Transfer failed");
+
+        estate.owner = msg.sender;
+        onSaleEstate.isSold = true;
+    }
+
+    function checkSellAd (uint256 id) public view returns (uint256, address, uint256, bool) {
+        return (onSaleAds[id].estateId, onSaleAds[id].seller, onSaleAds[id].price, onSaleAds[id].isSold);
     }
 }
